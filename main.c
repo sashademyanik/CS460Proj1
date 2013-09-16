f088c7d897ff3614bd574eb0fc51d9dcac9153e3
Riein <ttsvan@msn.com>
2013-09-16 16:36:33 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,101 +1,207 @@
-/*
+/* 
+ * Corey Amoruso, Michael Swiger, Sasha Demyanik
  * Eggshell
- * CS460
+ * CS460 Project 1
  */
 
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-#include<ctype.h>
-#define DEBUG 1
-#define MAXARG 5
-
-char *command;
-char **arguments;
-int id;
-
-void initArgArray(void);
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
-//Method Main: runs loop giving user a prompt to enter a command and its arguments, until the keyword 'exit' is recieved
+#define DEBUG      0
+#define DIRSIZE    1024
+#define BUFSIZE    500
+#define MAXARG     5
+#define MAXPATH    5
+#define PATH_DELIM ":"
+#define ARG_DELIM  " \n\r"
+
+
+/*
+ * Check if specified file exists.
+ * Code snippet from: http://stackoverflow.com/a/230070
+ */
+int fileExists(const char *filename) {
+	struct stat buffer;
+	return (stat(filename, &buffer) == 0);
+}
+
+
+/* Merge a path and a filename into one string. */
+char *mergePath(const char *path, const char *file) {
+	int totalLen = strlen(path) + strlen(file) + 2;
+	int pathLen = strlen(path);
+	
+	/* Allocate merged string to be length of path+file+slash+null. */
+	char *merged = malloc(sizeof(char) * totalLen);
+	memset(merged, '\0', totalLen);
+	
+	/* Copy path and file to merged string. */
+	strncpy(merged, path, pathLen);
+	strncpy(&merged[pathLen + 1], file, strlen(file));
+	
+	/* Add slash between path and file. */
+	merged[pathLen] = '/';
+	
+	return merged;
+}
+
+
+/* Check the given path for the given file. */
+int checkPath(const char *path, const char *file) {
+	char *merged = mergePath(path, file);
+	
+	/* Check if merged path exists. */
+	int r = fileExists(merged);
+	
+	free(merged);
+	
+	return r;
+}
+
+
+/*
+ * Main Function: runs loop giving user a prompt to enter a command and its 
+ * arguments, until the keyword 'exit' is recieved
+ */
 int main(int argc, char *argv[]){
+	char buffer[BUFSIZE];        /* input buffer with command + args */
+	char cwd[DIRSIZE];           /* current working directory */
+	char *path = getenv("PATH"); /* PATH env var */
+	char *execPath;              /* PATH where the command is located */	
+	char *token;                 /* used for tokenizing args and paths */
+	int pid;                     /* parent process id (for forking) */
+	
+	/* List of arguments parsed from buffer. */
+	char **arguments = malloc(sizeof(char*) * MAXARG);
+	int maxArgs = MAXARG;
+	int numArgs = 0;
+	
+	/* List of paths parsed from PATH. */
+	char **paths = malloc(sizeof(char*) * MAXPATH);
+	int numPaths = 0;
+	int maxPaths = MAXPATH;
 
-	char buffer[500];
-	int argumentCount = 0;
-	char *argBuffer;
+	/* Split the path env var into an array with individual paths. */
+	token = strtok(path, PATH_DELIM);
+	
+	do {
+		/* Double the size of the paths array if it's full. */
+		if (numPaths >= maxPaths - 1) {
+			maxPaths *= 2;
+			paths = realloc(paths, sizeof(char*) * maxPaths);
+		}
+		
+		paths[numPaths] = token;
+		numPaths++;
+	} while ((token = strtok(NULL, PATH_DELIM)));
+	
+	/* Debug info for checking paths. */
+	if (DEBUG) {
+		for (int i = 0; i < numPaths; i++) {
+			printf("Path %d: %s\n", i, paths[i]);
+		}
+	}
 
+	/* Continuously prompt for and execute commands until "exit" is input. */
+	while (1){
+		numArgs = 0;     /* reset current arg count */
+		execPath = NULL; /* reset execution path */
+		
+		/* Reset CWD. */
+		getcwd(cwd, DIRSIZE);
+		
+		/* Null out arguments list. */
+		for (int i = 0; i < maxArgs; i++) {
+			arguments[i] = NULL;
+		}
 
-	while(1){
+		/* Print prompt and read in command line. */
+		
+		printf("esh/:-> ");
+		fgets(buffer, BUFSIZE, stdin);
 
-		int maxArg = MAXARG;   //vriable used to resize argument array in the case more than MAXARG arguments are supplied
-		initArgArray();        //initiates the argument array
-
-		printf("esh/:-> ");    //print prompt
-		fgets(buffer,500,stdin); //read in command and arguments
-		argumentCount = 0;     //set argument count to 0
-
-		command = strtok(buffer, " \n\r");  //tokenize command from input buffer
-
-		argBuffer = strtok(NULL," \n\r");  //tokenize first argument (if not present, returns NULL)
-
-
-		//check for exit command, breaks from loop
-		if(!strcmp(command,"exit")){
+		/* Pull the command from the input buffer. */
+		token = strtok(buffer, ARG_DELIM);
+		
+		/* Tokenize arguments from buffer and store in arguments array. */
+		do {
+			/* Double the size of the arguments array if it's full. */
+			if(token != NULL && numArgs >= maxArgs - 1){
+				maxArgs *= 2;
+				arguments = realloc(arguments, (maxArgs * sizeof(char*)));
+			}
+			
+			arguments[numArgs] = token;
+			numArgs++;
+		} while ((token = strtok(NULL, ARG_DELIM)));
+		
+		/* Check for exit, break the loop if specified. */
+		if(!strcmp(arguments[0], "exit")){
 			free(arguments);
 			break;
 		}
 
-		//loop to tokenize arguments and store them in arguments array
-		while(argBuffer != NULL){
-			arguments[argumentCount] = strdup(argBuffer);  //assign argument to array
-			argBuffer = strtok(NULL," \n\r");              //tokenize next argument
-			argumentCount++;                               //increment argument counter
-		
-
-			//doubles the size of the argument array when more than MAXARG arguments are entered.	 
-			if(argBuffer != NULL && argumentCount > maxArg){ //checks to make sure another arg exists
-				maxArg = (argumentCount - 1) * 2; //set size of new array (double the original)
-				arguments =  realloc(arguments,(maxArg * sizeof(char*))); //reallocate arguments array
+		/* Debug info for verifying command and arguments. */
+		if (DEBUG){
+			printf("Command: %s \n", arguments[0]);
+			for (int i = 1; i < numArgs; i++){
+				printf("Argument %d: %s \n", i, arguments[i]);
 			}
 		}
-
-
-              //---------------------------------------------------------------------
-	     // test case for verifying command and arguments	                  //|
-	  	if(DEBUG){                                                        //|
-			printf("Command: %s \n", command);			  //|
-			for(int i = 0; i < argumentCount; i++){                   //|
-				printf("Argument %d: %s \n", i, arguments[i]);    //|
-			}                                                         //|
-		}                                                                 //|
-		//--------------------------------------------------------------------
-
-
-		//Fork child processs
-		//if(id = fork()){                      (un-comment this line)
-			//parent portion of fork	
-	//	}
-		//else{					(and this one)
-			//child prtion of fork
-
-	//	}
-
-		free(arguments);  //free argument array
+		
+		/* Check if command exists in environment paths. */
+		for (int i = 0; i < numPaths; i++) {
+			if (checkPath(paths[i], arguments[0])) {
+				execPath = paths[i];
+			}
+		}
+		
+		/* Check if command exists in cwd (if not found in paths). */
+		if (!execPath && checkPath(cwd, arguments[0])) {
+			execPath = cwd;
+		}
+		
+		/* If the file was not found at any path, check for new input. */
+		if (!execPath) {
+			printf("Command `%s' not found.\n", arguments[0]);
+			continue;
+		}
+		
+		/* If it was found, fork and execute. */
+		pid = fork();
+		
+		/* 
+		 * If this is the parent branch and & isn't last arg, wait for child
+		 * to die.
+		 */
+		if (pid != 0) {
+			if(strcmp("&", arguments[numArgs - 1]) == 0){
+				continue;
+			}else{
+				wait(NULL);
+			}
+			
+		}
+		
+		/* If this is the child branch, execute the command. */
+		else {
+			if (strcmp("&", arguments[numArgs - 1]) == 0) {
+				arguments[numArgs - 1] = NULL;
+			}
+			
+			char *merged = mergePath(execPath, arguments[0]);
+			execv(merged, arguments);
+			exit(0);
+		}
 	}
 	
 	return 0;
 }
-
-//------------------------------------------------------------------------------------------------------------------
-/*Method initArgArray: allocates memory for arguments array and init values to NULL*/
-void initArgArray(void){
-	arguments = malloc(MAXARG * sizeof(char*));
-	for(int i = 0; i < MAXARG; i++){
-		arguments[i] = NULL;
-	}
-}
-
-
-
